5871bc8f9f6fd2fbd28d7f72115e9f59e742f95d
Dapo Olasiyan <Dapo@dapos-mbp.home>
2020-05-30 13:55:17 +0100

    Update my latest exercises

---
+++ Dapo/thorexercises/thorFirstSunday.js
@@ -0,0 +1,22 @@
+let today = new Date("2014-01-01");
+let year = today.getFullYear();
+let month = today.getMonth();
+let day = today.getDate();
+let i=0;
+let x;
+
+do  {
+    c = new Date(year + i, month, day);
+    i++;
+    x = c.getDay();
+    }
+
+while (x !=0);
+console.log (c);
+console.log (x);
+
+
+ // do {
+    // code block to be executed
+  //}
+ // while (condition);
