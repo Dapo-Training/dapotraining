61591545f67b0b694e9cac2d78a714519ac9b7f7
Dapo Olasiyan <Dapo@dapos-mbp.home>
2020-06-10 14:03:51 +0100

    adding classattempt.js file

---
+++ classattempt.js
@@ -0,0 +1,11 @@
+class Person{
+    constructor(gender) {
+      this.personsex = gender;
+    }
+
+  }
+  
+  myperson = new Person("Fluid");
+  console.log (myperson.personsex + ' ' + myperson.personage);
+  
+    
